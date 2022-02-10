eca4895c5abf3218c8d3eb163e655654351ea3ae
tolulonge <tolulopelonge945@gmail.com>
2022-02-10 03:15:12 +0100

    sharedPrefs to hold userInfo setup complete

---
+++ app/src/main/java/com/tolulonge/dietcounter/DietCounterApp.kt
@@ -0,0 +1,7 @@
+package com.tolulonge.dietcounter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DietCounterApp: Application()
---
+++ app/src/main/java/com/tolulonge/dietcounter/di/AppModule.kt
@@ -0,0 +1,37 @@
+package com.tolulonge.dietcounter.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.tolulonge.core.data.preferences.DefaultPreferences
+import com.tolulonge.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application
+    ): SharedPreferences {
+        return app.getSharedPreferences("shared_pref", MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
+        return DefaultPreferences(sharedPreferences)
+    }
+
+//    @Provides
+//    @Singleton
+//    fun provideFilterOutDigitsUseCase(): FilterOutDigits {
+//        return FilterOutDigits()
+//    }
+}
