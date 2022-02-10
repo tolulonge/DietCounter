dc9ce1954de235ea6848623440f203dda9a041b4
tolulonge <tolulopelonge945@gmail.com>
2022-02-10 01:03:13 +0100

    welcome screen implemented

--- app/src/main/java/com/tolulonge/dietcounter/MainActivity.kt
+++ app/src/main/java/com/tolulonge/dietcounter/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.tolulonge.dietcounter.ui.theme.DietCounterTheme
+import com.tolulonge.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DietCounterTheme {
-
+                WelcomeScreen()
             }
         }
     }
